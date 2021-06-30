b7ede702716e96a8be948cdbfb746d103f7b1ef1
Orkhan <jkrogers222@gmail.com>
2021-06-30 19:46:48 -0400

    for loop

--- src/test/java/UserStory100.java
+++ src/test/java/UserStory100.java
@@ -2,5 +2,8 @@ public class UserStory100 {
     public static void main(String[] args) {
 
         System.out.println("First user story100");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i++);
+        }
     }
 }
