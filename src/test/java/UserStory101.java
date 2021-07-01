00745ada57bf4e45c43bf3eae3ce509ab799f58a
Orkhan <jkrogers222@gmail.com>
2021-06-30 20:21:37 -0400

    usa now

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -3,5 +3,6 @@ public class UserStory101 {
         System.out.println("New user story...");
         System.out.println("New user story...in progress");
         System.out.println("Pushing without merging");
+        System.out.println("USA");
     }
 }
