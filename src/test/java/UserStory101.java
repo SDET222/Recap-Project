69419148dc4342a7fa39665c5ecf0349aab022a1
Orkhan <jkrogers222@gmail.com>
2021-06-30 20:20:31 -0400

    working on us101 in progress

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -1,5 +1,6 @@
 public class UserStory101 {
     public static void main(String[] args) {
         System.out.println("New user story...");
+        System.out.println("New user story...in progress");
     }
 }
